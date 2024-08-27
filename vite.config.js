2b94b04679ec46d842f487c08078b9db2316742b
Shreeraj Parmar <147899890+Shreeraj-Parmar@users.noreply.github.com>
2024-08-27 19:09:42 +0530

    Update vite.config.js
    0.0.0.0 added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000, // Use Render's port or fallback to 3000
+  },
+};
